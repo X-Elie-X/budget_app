9459bd247c709cf14e00f2acea65d32e45e8a85d
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-12 20:06:10 +0200

    installing gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
-
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
