8f69faa6baf98bd227b77269defb4e9a9a5dee2b
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-12 20:11:39 +0200

    association's

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+    has_many :group_budgets, dependent: :destroy
+    has_many :budgets, through: :group_budgets
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :budgets, dependent: :destroy
+  has_many :group_budgets, through: :budgets, dependent: :destroy
+  has_many :groups, through: :group_budgets, dependent: :destroy
 end
