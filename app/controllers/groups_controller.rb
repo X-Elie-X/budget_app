bac4386b9ab3d147836bc6e955083d0eac505122
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-12 20:18:11 +0200

    add controllers

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,25 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'budgets/index'
+  get 'budgets/new'
+  get 'budgets/show'
+  get 'budgets/create'
+  get 'budgets/edit'
+  get 'budgets/update'
+  get 'budgets/destroy'
+  get 'budgets/search'
+  get 'groups/index'
+  get 'groups/new'
+  get 'groups/show'
+  get 'groups/create'
+  get 'groups/edit'
+  get 'groups/update'
+  get 'groups/destroy'
+  get 'groups/search'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  get 'users/search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
