dc868d099609411eee98c768a5ad86cd8c0dda3c
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-12 22:29:42 +0200

    install devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable 
     has_many :budgets, dependent: :destroy
   has_many :group_budgets, through: :budgets, dependent: :destroy
   has_many :groups, through: :group_budgets, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,7 @@
 Rails.application.routes.draw do
-  get 'budgets/index'
-  get 'budgets/new'
-  get 'budgets/show'
-  get 'budgets/create'
-  get 'budgets/edit'
-  get 'budgets/update'
-  get 'budgets/destroy'
-  get 'budgets/search'
-  get 'groups/index'
-  get 'groups/new'
-  get 'groups/show'
-  get 'groups/create'
-  get 'groups/edit'
-  get 'groups/update'
-  get 'groups/destroy'
-  get 'groups/search'
-  get 'users/index'
-  get 'users/new'
-  get 'users/show'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  get 'users/search'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
