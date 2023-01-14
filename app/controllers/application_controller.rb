5a7c6bfa7ab6397d1f7726de34203516bf0f61b5
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2023-01-14 17:00:58 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:name, :email, :password, :password_confirmation, :role)
